e1b17811d2d2545b977946af3a83db1777d4c667
Maxwel5 <maxwellwafulasim@gmail.com>
2019-09-09 05:57:49 +0300

    Defining article function to view article pages

--- app/main/views.py
+++ app/main/views.py
@@ -1,17 +1,27 @@
 from flask import render_template
-from app import app
+from . import main
 
 # Views
-@app.route('/')
+@main.route('/')
 def index():
 
     '''
     View root page function that returns the index page and its data
     '''
-    title = "News Highlighter"
+    title = "News highlight"
     bussiness_sources = get_sources('bussiness')
     world_sources = get_sources('world')
     entertainment_sources = get_sources('entertainment')
     sports_sources = get_sources('sports')
     
-    render_template('index.html',title = title,bussiness_sources = bussiness_sources,world_sources = world_sources,entertainment_sources = entertainment_sources,sports_sources = sports_sources)
+    return render_template('index.html',title = title,bussiness_sources = bussiness_sources,world_sources = world_sources,entertainment_sources = entertainment_sources,sports_sources = sports_sources)
+
+@main.route('sources/<id>')
+def index(id):
+    '''
+    article page to be viewed
+    '''
+    articles = get_articles(id)
+    title =f'News highlight | {id}'
+
+    return render_template('article.html',title = title,articles = articles)
