f5504095690f8391b4782ed6f96903d22904a09d
Maxwel5 <maxwellwafulasim@gmail.com>
2019-09-09 16:20:21 +0300

    Defining source model

--- app/models.py
+++ app/models.py
@@ -2,4 +2,12 @@ class News_Sources:
     '''
     This class is meant to define sources objects
     '''
-    
+
+    def __init__(self,id,title,illustration,url,group,nation,language):
+        self.id = id
+        self.title = title
+        self.illustration = illustration
+        self.url = url
+        self.group = group
+        self.nation = nation
+        self.language = language
