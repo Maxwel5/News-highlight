ef5fbc19ea1584c3353c6ea12d831aedaff6cb2e
Maxwel5 <maxwellwafulasim@gmail.com>
2019-09-09 10:54:04 +0300

    Defining the function that processes the articles and converts them to a list of objects

--- app/request.py
+++ app/request.py
@@ -62,25 +62,20 @@ def process_results(news_sources_list):
 
     return news_sources_results
 
-def get_movie(id):
-    get_movie_details_url = base_url.format(id,api_key)
+def get_articles(id):
+    '''
+    Function  that processes the articles result and converts them to a list of Objects
+    '''
+    get_articles_url = base_url.format(id,api_key)
 
-    with urllib.request.urlopen(get_movie_details_url) as url:
-        movie_details_data = url.read()
-        movie_details_response = json.loads(movie_details_data)
+    with urllib.request.urlopen(get_articles_details_url) as url:
+        articles_data = url.read()
+        articles_response = json.loads(articles_data)
 
-        movie_object = None
-        if movie_details_response:
-            id = movie_details_response.get('id')
-            title = movie_details_response.get('original_title')
-            overview = movie_details_response.get('overview')
-            poster = movie_details_response.get('poster_path')
-            vote_average = movie_details_response.get('vote_average')
-            vote_count = movie_details_response.get('vote_count')
-
-            movie_object = Movie(id,title,overview,poster,vote_average,vote_count)
-
-    return movie_object
+        articles_object = None
+        if articles_response['articles']:
+            
+    return articles_object
 
 def search_movie(movie_name):
     search_movie_url = 'https://api.themoviedb.org/3/search/movie?api_key={}&query={}'.format(api_key,movie_name)
