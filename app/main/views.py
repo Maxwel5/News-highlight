5c10a52805e42ad73fcd203c1b3314ef3b7157c2
Maxwel5 <maxwellwafulasim@gmail.com>
2019-09-09 05:42:59 +0300

    Enabling the route function that returns the index page and its data

--- app/main/views.py
+++ app/main/views.py
@@ -8,4 +8,10 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    title = "News Highlighter"
+    bussiness_sources = get_sources('bussiness')
+    world_sources = get_sources('world')
+    entertainment_sources = get_sources('entertainment')
+    sports_sources = get_sources('sports')
+    
+    render_template('index.html',title = title,bussiness_sources = bussiness_sources,world_sources = world_sources,entertainment_sources = entertainment_sources,sports_sources = sports_sources)
